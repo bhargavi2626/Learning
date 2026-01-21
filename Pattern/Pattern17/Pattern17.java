0fe9e4bba282128ca031bfff86ea320e252d0c1f
SKBhargavi <bhargavisk12@gmail.com>
2026-01-21 13:42:47 +0530

    push the 20 and 19

--- Pattern/Pattern17/Pattern17.java
+++ Pattern/Pattern17/Pattern17.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Pattern19 {
+class Pattern17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
---
+++ Pattern/Pattern19/Pattern19.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Pattern19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
